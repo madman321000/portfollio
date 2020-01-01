733942b97c0821bdf89b3b1e6970e236f2109f91
madman321000 <maneesh.madala@gmail.com>
2020-01-01 17:29:20 -0500

    Updates

---
+++ precache-manifest.838fcd89014639817c71026334026154.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c76912e5938ccd3ad6b442f9f81e7df",
+    "url": "/portfollio/index.html"
+  },
+  {
+    "revision": "a3c6f8c7248dcda9b6b5",
+    "url": "/portfollio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4fe822df4cd261f6f474",
+    "url": "/portfollio/static/js/2.b13f70de.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfollio/static/js/2.b13f70de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3c6f8c7248dcda9b6b5",
+    "url": "/portfollio/static/js/main.4078748d.chunk.js"
+  },
+  {
+    "revision": "7f6eab08d3c4fb58e746",
+    "url": "/portfollio/static/js/runtime-main.ede4dbd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfollio/static/media/logo.5d5d9eef.svg"
+  }
+]);
